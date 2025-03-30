7fb722514f63584e68ebb10f262f247cf7030137
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2025-03-30 23:55:42 +0300

    "Backup 2025-03-30 23:55:42.930301"

--- dotfiles.py
+++ dotfiles.py
@@ -16,29 +16,34 @@ dotfiles = [
 HOME = os.environ['HOME']
 THIS = os.path.dirname(os.path.realpath(__file__))
 
+def copy_all(src_dir, dst_dir):
+    for path in dotfiles:
+        src = os.path.join(src_dir, path)
+        dst = os.path.join(dst_dir, path)
+        print(f'Copying {src} to {dst}')
+        if os.path.isfile(src):
+            shutil.copy2(src, dst)
+        else:
+            shutil.copytree(src, dst, dirs_exist_ok=True)
+
 def install():
-    pass
+    print('Installing dotfiles from backup...')
+    subprocess.run(['git', 'pull', 'origin', 'main'], cwd=THIS)
+    copy_all(THIS, HOME)
+    print('Dotfiles installation finished')
 
 def backup():
     print('Start backup of dotfiles...')
-    # todo add remove
-    for path in dotfiles:
-        src = os.path.join(HOME, path)
-        print(f'\tCopying {src}')
-        if os.path.isfile(src):
-            shutil.copy2(src, THIS)
-        else:
-            target = os.path.join(THIS, path)
-            shutil.copytree(src, target, dirs_exist_ok=True)
+    copy_all(HOME, THIS)
     subprocess.run(['git', 'add', '.'], cwd=THIS)
     subprocess.run(['git', 'commit', '-m', f'"Backup {datetime.datetime.now()}"'], cwd=THIS)
     subprocess.run(['git', 'push', 'origin', 'main'], cwd=THIS)
-    print('Finish backup of dotfiles')
+    print('Backup of dotfiles finished')
 
 def main():
     args = sys.argv
     if 'install' in args:
-        print('kek')
+        install()
     elif 'backup' in args:
         backup()
     else:

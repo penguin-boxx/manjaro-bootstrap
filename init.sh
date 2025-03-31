433930e6e645272795f5a4530ba30c7ad4eaeb71
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2025-03-31 12:43:39 +0300

    Update

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@ sudo pacman-mirrors -f3
 sudo pacman -Syyu # update system
 
 sudo pacman -S \
-    wget links lynx cmatrix sl cowsay elinks gparted gnome-system-monitor nautilus htop mc tree gnome-calculator gnome-search-tool gedit xf86-input-wacom　linux-headers xinput cheese gcolor3 screenfetch keepassxc \
+    xf86-input-wacom linux-headers-meta xorg-xinput \
+    htop mc tree wget links lynx cmatrix sl cowsay elinks gparted gnome-system-monitor nautilus gnome-calculator gedit cheese gcolor3 screenfetch keepassxc \
     zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb \
-    firefox chromium discord telegram-desktop zoom qbittorrent \
-    code code-marketplace dbeaver vim \
-    git gitk cmake boost gtest doxygen stack ocaml opam rust rust-docs rustup mypy flake8 ipython python-pip autopep8 kotlin maven gradle junit \
+    firefox chromium discord telegram-desktop qbittorrent \
+    code dbeaver vim \
+    git cmake boost gtest doxygen stack ocaml opam rust rustup mypy flake8 ipython python-pip autopep8 kotlin maven gradle junit \
     virtualbox docker valgrind postgresql \
     libreoffice xournalpp shotwell krita inkscape openshot vlc blender fbreader evince gimp coolreader obs-studio \
     texstudio texlive texlive-lang texlive-bibtexextra biber

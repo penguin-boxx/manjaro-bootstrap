980fbe3d32db91bdf861b315ef77f700c0b7072e
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2025-03-31 14:02:41 +0300

    Update

--- init.sh
+++ init.sh
@@ -5,14 +5,15 @@ sudo pacman -Syyu # update system
 
 sudo pacman -S \
     xf86-input-wacom linux-headers-meta xorg-xinput \
-    htop mc tree wget links lynx cmatrix sl cowsay elinks gparted gnome-system-monitor nautilus gnome-calculator gedit cheese gcolor3 screenfetch keepassxc \
+    htop mc tree wget links lynx cmatrix sl cowsay elinks gparted gnome-system-monitor baobab nautilus gnome-calculator gedit cheese gcolor3 screenfetch keepassxc \
     zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb \
     firefox chromium discord telegram-desktop qbittorrent \
-    code dbeaver vim intellij-idea-community-edition \
-    git cmake boost gtest doxygen stack ocaml opam rust rustup mypy flake8 ipython python-pip autopep8 kotlin maven gradle junit npm nvm \
+    code obsidian vim intellij-idea-community-edition dbeaver \
+    git cmake boost gtest doxygen stack ocaml opam rust rustup mypy flake8 ipython python-pip autopep8 kotlin maven gradle junit sbt npm nvm \
     virtualbox docker valgrind postgresql \
     libreoffice xournalpp shotwell krita inkscape openshot vlc blender fbreader evince gimp coolreader obs-studio \
-    texstudio texlive texlive-lang texlive-bibtexextra biber
+    texstudio texlive texlive-lang texlive-bibtexextra biber \
+    steam
 
 # change bash to zsh
 chsh -s $(which zsh)
